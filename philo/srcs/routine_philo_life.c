fa88a88a078cd0124e877fafa2850ed24344c51d
kaaaaakun_ <kaaaaakun_@kaaaaakun-noMacBook-Air.local>
2023-10-23 23:20:42 +0900

    daitaikansei

--- philo/srcs/routine_philo_life.c
+++ philo/srcs/routine_philo_life.c
@@ -22,7 +22,8 @@ int		is_other_philo_dead(t_mutex *mutex_struct);
 int		set_fork(t_philo_status *philosopher, pthread_mutex_t *fork[]);
 int		get_fork_and_eat_philo(t_philo_status *philosopher, pthread_mutex_t *fork[], long long int last_eat_time);
 int		set_deth_flag(int philo_id, t_mutex *mutex_struct);
-int		is_during_eating_philo_died(int philo_id, long long int last_eat_time, t_philo_routine_data *routine_data);
+//int		is_during_eating_philo_died(int philo_id, long long int last_eat_time, t_philo_routine_data *routine_data);
+long long int	get_time_left_of_philo_died(int philo_id, long long int last_eat_time, t_philo_routine_data *routine_data);
 
 int		set_fork(t_philo_status *philosopher, pthread_mutex_t *fork[])
 {
@@ -56,6 +57,7 @@ int		get_fork_and_eat_philo(t_philo_status *philosopher, pthread_mutex_t *fork[]
 {
     int						philo_id;
 	t_mutex					*mutex_struct;
+	long long int			time_left;
 
 	mutex_struct = philosopher->mutex_struct;
 	philo_id = philosopher->philo_id;
@@ -67,31 +69,19 @@ int		get_fork_and_eat_philo(t_philo_status *philosopher, pthread_mutex_t *fork[]
 		pthread_mutex_unlock(fork[0]);
 		return (-1);
 	}
-	if (is_during_eating_philo_died(philo_id, last_eat_time, philosopher->routine_data) == -1)
-	{
-		set_deth_flag(philo_id, mutex_struct);
-		pthread_mutex_unlock(fork[0]);
-		return (-1);
-	}
 	m_printf(FORK, philo_id, 1, mutex_struct);
 	pthread_mutex_lock(fork[1]);
 	m_printf(FORK, philo_id, 1, mutex_struct);
 	m_printf(EAT, philo_id, 1, mutex_struct);
-	if (philo_id == philosopher->routine_data->num_of_philo - 1)
+	
+	time_left = get_time_left_of_philo_died(philo_id, last_eat_time, philosopher->routine_data);//死ぬまでの時間
+	if (time_left < philosopher->routine_data->time_to_eat / 1000)//死ぬまでの時間＜食べる時間
 	{
-		pthread_mutex_lock(&mutex_struct->eat_count_mutex);	
-		mutex_struct->eat_count++;
-		if (mutex_struct->eat_count == philosopher->routine_data->number_of_times_each_philosopher_must_eat)
-		{
-			pthread_mutex_lock(&mutex_struct->deth_flag_mutex);
-			mutex_struct->deth_flag = DEAD;
-			pthread_mutex_unlock(&mutex_struct->deth_flag_mutex);
-			pthread_mutex_unlock(&mutex_struct->eat_count_mutex);	
-			pthread_mutex_unlock(fork[1]);
-			pthread_mutex_unlock(fork[0]);
-			return (-1);
-		}
-		pthread_mutex_unlock(&mutex_struct->eat_count_mutex);	
+		usleep(time_left * 1000);
+		set_deth_flag(philo_id, mutex_struct);//死亡判定
+		pthread_mutex_unlock(fork[1]);
+		pthread_mutex_unlock(fork[0]);
+		return (-1);
 	}
 	usleep(philosopher->routine_data->time_to_eat);
 	pthread_mutex_unlock(fork[1]);
@@ -147,7 +137,8 @@ int	set_deth_flag(int philo_id, t_mutex *mutex_struct)
 {
 	pthread_mutex_lock(&mutex_struct->deth_flag_mutex);
 	mutex_struct->deth_flag = DEAD;
-	m_printf(DIED, philo_id, 1, mutex_struct);
+	if (0 <= philo_id)
+		m_printf(DIED, philo_id, 1, mutex_struct);
 	pthread_mutex_unlock(&mutex_struct->deth_flag_mutex);
 	return (0);
 }
@@ -157,7 +148,9 @@ void	*routine_philo_life(void *philo_status)
 	t_philo_status	*philosopher;
 	long long int	last_eat_time;
     pthread_mutex_t	*fork[2];
+	int				i;
 
+	i = 0;
 	philosopher = (t_philo_status *)philo_status;
 	set_fork(philosopher, fork);
 	last_eat_time = only_get_ms_time();
@@ -165,21 +158,31 @@ void	*routine_philo_life(void *philo_status)
 	{
 		if (get_fork_and_eat_philo(philosopher, fork, last_eat_time) != SUCCESS)
 			return (NULL);
+		i++;
+		if (i == philosopher->routine_data->number_of_times_each_philosopher_must_eat)
+			break ;
 		last_eat_time = only_get_ms_time();
 		if (sleep_philo(philosopher) != SUCCESS)
 			return (NULL);
 		if (think_philo(philosopher) != SUCCESS)
 			return (NULL);
 	}
+	if (philosopher->philo_id == philosopher->routine_data->num_of_philo - 1)
+	{
+		usleep(100);
+		set_deth_flag(-1, philosopher->mutex_struct);//	擬似死亡判定
+	}
 	return (NULL);
 }
 
-int	is_during_eating_philo_died(int philo_id, long long int last_eat_time, t_philo_routine_data *routine_data)
+long long int	get_time_left_of_philo_died(int philo_id, long long int last_eat_time, t_philo_routine_data *routine_data)
 {
 	long long int now_time;
 	long long int time_left;
 
 	now_time = only_get_ms_time();
+	if (now_time == -1)
+		return (-1);
 	time_left = routine_data->time_to_die / 1000 - (now_time - last_eat_time);
 	if (DEBUG == 1)
 		dprintf(2,"id : %d \n \
@@ -190,10 +193,7 @@ int	is_during_eating_philo_died(int philo_id, long long int last_eat_time, t_phi
 		now_time - last_eat_time, time_left, \
 		routine_data->time_to_die / 1000, \
 		routine_data->time_to_eat / 1000, routine_data->time_to_sleep / 1000);
-	if (time_left <= routine_data->time_to_eat / 1000)
-		return (-1);
-	else
-		return (0);
+	return (time_left);
 	(void)routine_philo_life;
 }
 
